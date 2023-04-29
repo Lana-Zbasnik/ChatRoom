ce9e378e6f8748af1134028ebc9ef4b5b303ddee
Lana-Zbasnik <115007072+Lana-Zbasnik@users.noreply.github.com>
2023-04-29 03:52:58 -0600

    Add files via upload

---
+++ server/Server.java
@@ -0,0 +1,61 @@
+/**
+ * An echo server listening on port 6007. 
+ * This server reads from the client
+ * and echoes back the result. 
+ *
+ * This services each request in a separate thread.
+ *
+ * This conforms to RFC 862 for echo servers.
+ *
+ * @author - Greg Gagne.
+ */
+
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.concurrent.*;
+
+public class  Server
+{
+	public static final int DEFAULT_PORT =5555;
+
+    // construct a thread pool for concurrency	
+	private static final Executor exec = Executors.newCachedThreadPool();
+	
+	public static void main(String[] args) throws IOException {
+		ServerSocket sock = null;
+		
+		try {
+			// establish the socket
+			sock = new ServerSocket(DEFAULT_PORT);
+			
+			Vector<String> messageQueue = new Vector<String>();
+			ArrayList<BufferedWriter> clients= new ArrayList<BufferedWriter>();
+			
+			// Broadcast Thread
+			Runnable broadcast = new BoradcastThread(messageQueue, clients);
+			exec.execute(broadcast);
+			
+			while (true) {
+				/**
+				 * now listen for connections
+				 * and service the connection in a separate thread.
+				 */
+				
+				Socket client = sock.accept();
+				BufferedWriter fromWriter = new  BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+				BufferedWriter toClient = new BufferedWriter(fromWriter);
+				clients.add(toClient);
+				
+				Runnable task = new Connection(sock.accept(), messageQueue );
+				exec.execute(task);
+			}
+		}
+		catch (IOException ioe) { System.err.println(ioe); }
+		finally {
+			if (sock != null)
+				sock.close();
+		}
+	}
+}
